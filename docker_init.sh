2555c2d4e3da37d000ca4fb79a18f54665a831bb
Peter Rounce <peter@rounce.me.uk>
2024-06-14 06:15:02 +0000

    fix multiple inits

--- docker_init.sh
+++ docker_init.sh
@@ -5,6 +5,8 @@ read domain_name
 echo Enter the GroundControl URL excluding the protocol
 read gc_url
 
+cp docker/card/Dockerfile.template docker/card/Dockerfile
+
 echo writing the domain name as $domain_name ..
 sed -i "s/domain_name/https:\/\/$domain_name/" Caddyfile
 sed -i "s/domain_name/$domain_name/g" docker/card/Dockerfile
