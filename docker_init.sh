8d2db3063fb3d465c522a4621c6e4a4d03f403c9
Peter Rounce <peter@rounce.me.uk>
2024-07-09 08:15:26 +0100

    simplify docker init

--- docker_init.sh
+++ docker_init.sh
@@ -2,15 +2,9 @@
 echo Enter the domain name excluding the protocol
 read domain_name
 
-echo Enter the GroundControl URL excluding the protocol
-read gc_url
-
 cp Caddyfile.template Caddyfile
 cp docker/card/Dockerfile.template docker/card/Dockerfile
 
 echo writing the domain name as $domain_name ..
 sed -i "s/domain_name/https:\/\/$domain_name/" Caddyfile
 sed -i "s/domain_name/$domain_name/g" docker/card/Dockerfile
-
-echo writing the gc url as $gc_url ..
-sed -i "s/gc_url/$gc_url/g" docker/card/Dockerfile
