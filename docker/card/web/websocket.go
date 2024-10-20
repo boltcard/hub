d3d741edebe268066ebcb3611fab4c01ae0e6e32
Peter Rounce <peter@rounce.me.uk>
2024-10-20 12:54:40 +0000

    handle phoenix not available

--- docker/card/web/websocket.go
+++ docker/card/web/websocket.go
@@ -54,52 +54,55 @@ func WebsocketHandler(w http.ResponseWriter, r *http.Request) {
 	// https://github.com/gorilla/websocket/issues/209#issuecomment-275419998
 	h := http.Header{"Authorization": {"Basic " + base64.StdEncoding.EncodeToString([]byte(":"+hp))}}
 	c, _, err := websocket.DefaultDialer.Dial("ws://phoenix:9740/websocket", h)
-	util.Check(err)
-	defer c.Close()
+	if err != nil {
+		log.Info("phoenix not available : ", err.Error())
+	} else {
+		defer c.Close()
 
-	done := make(chan struct{})
+		done := make(chan struct{})
 
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := c.ReadMessage()
-			if err != nil {
-				log.Info("websocket to phoenix is closing : ", err.Error())
-				return
+		go func() {
+			defer close(done)
+			for {
+				_, message, err := c.ReadMessage()
+				if err != nil {
+					log.Info("websocket to phoenix is closing : ", err.Error())
+					return
+				}
+
+				// `message` contains the websocket message as []byte from Phoenix server
+				log.Info("message : ", string(message))
+
+				// decode the JSON into a struct
+				var webSocketMessage WebSocketMessage
+
+				err = json.Unmarshal(message, &webSocketMessage)
+				util.Check(err)
+
+				log.Info("webSocketMessage : ", webSocketMessage)
+
+				// look up the payment_hash to look up the description using GetIncomingPayment
+				incomingPayment, err := phoenix.GetIncomingPayment(webSocketMessage.PaymentHash)
+				util.Check(err)
+
+				log.Info("incomingPayment : ", incomingPayment)
+
+				// TODO: send JSON encoded data to add a tx row
+				// TODO: send JSON encoded data to update the totals
+				now := time.Now()
+				now_string := now.Format("15:04:05")
+				err = conn.WriteMessage(websocket.TextMessage,
+					[]byte(now_string+" UTC, "+
+						strconv.Itoa(incomingPayment.ReceivedSat)+" sats received, "+
+						strconv.Itoa(incomingPayment.Fees)+" sats fees,"+
+						" message: "+webSocketMessage.PayerNote))
+				if err != nil {
+					log.Warning("websocket write error :", err)
+					return
+				}
 			}
-
-			// `message` contains the websocket message as []byte from Phoenix server
-			log.Info("message : ", string(message))
-
-			// decode the JSON into a struct
-			var webSocketMessage WebSocketMessage
-
-			err = json.Unmarshal(message, &webSocketMessage)
-			util.Check(err)
-
-			log.Info("webSocketMessage : ", webSocketMessage)
-
-			// look up the payment_hash to look up the description using GetIncomingPayment
-			incomingPayment, err := phoenix.GetIncomingPayment(webSocketMessage.PaymentHash)
-			util.Check(err)
-
-			log.Info("incomingPayment : ", incomingPayment)
-
-			// TODO: send JSON encoded data to add a tx row
-			// TODO: send JSON encoded data to update the totals
-			now := time.Now()
-			now_string := now.Format("15:04:05")
-			err = conn.WriteMessage(websocket.TextMessage,
-				[]byte(now_string+" UTC, "+
-					strconv.Itoa(incomingPayment.ReceivedSat)+" sats received, "+
-					strconv.Itoa(incomingPayment.Fees)+" sats fees,"+
-					" message: "+webSocketMessage.PayerNote))
-			if err != nil {
-				log.Warning("websocket write error :", err)
-				return
-			}
-		}
-	}()
+		}()
+	}
 
 	for {
 		// read message from client
